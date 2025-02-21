b88d955fc8e827bc3d0eb122eff5bcd7647ff893
Renan Leite <renan.vbleite@gmail.com>
2025-02-21 00:58:10 -0300

    add cart-item route to app.ts

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,7 @@ import customerRoutes from './routes/customerRoutes'
 import cartRoutes from './routes/cartRoutes'
 import authRoutes from './routes/authRoutes'
 import {authenticate} from './middlewares/AuthMiddleware'
+import cartItemRoutes from './routes/cartItemRoutes'
 
 const app = express()
 const port = 3000
@@ -20,11 +21,12 @@ app.use(authenticate)
 app.use('/products', productRoutes)
 app.use('/customers', customerRoutes)
 app.use('/carts', cartRoutes)
+app.use('/cart-items', cartItemRoutes)
 
 const startServer = async () => {
     try {
         await sequelize.authenticate()
-        await sequelize.sync({force: true}) // create database tables if don't exist
+        // await sequelize.sync({force: false}) // create database tables if don't exist
         console.log('Database connection established successfully.')
 
         app.listen(port, () => {
